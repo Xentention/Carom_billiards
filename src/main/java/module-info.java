6d09ad208d55f7e9ea54f2383e0243f78582e979
Xenia <xentimoshenko@gmail.com>
2022-12-03 15:05:36 +0300

    fixed:     - git ignoring some resources files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.carom_billiards {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.media;
+
+    opens coursework.carom_billiards to javafx.fxml;
+    exports coursework.carom_billiards;
+}
